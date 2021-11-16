44d84d34427eb0454984ecfe199324187040ad3d
cheokjw <cheok1121@gmail.com>
2021-11-16 17:07:35 +0800

    tried to add pointer function

--- assignment.cpp
+++ assignment.cpp
@@ -1,20 +1,89 @@
-// assignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <iomanip>
+
 
 using namespace std;
 
 
-void Pass_by_value(int arr[], int size) {
+// initializing functions
+void pass_by_value(int val[], int N);
+
+
+
+
+int main()
+{
+
+	// initializing valriables
+	int choice, num, arr[99], i, sorted_arr;
+
+
+	// Ask user to choose Pass-by-value or Pass-by Reference
+	cout << "Welcome to our Bubble Sort Program :)";
+	cout << "\nPlease choose\n1. Pass-By-Value\n2. Pass-by-Address\nChoice : ";
+	cin >> choice;
+
+
+	// input
+	cout << "enter total number of elements: ";
+	cin >> num;
+	cout << "\n enter " << num << " elements \n";
+
+
+	// letting the user to input all of the values into an array
+	for (i = 0; i < num; i++) {
+		cout << "number " << i + 1 << " : ";
+		cin >> arr[i];
+	}
+
+
+	// Before Sorting
+	for (i = 0; i < num; i++) {
+		cout << arr[i] << " ";
+
+	}
+
+	cout << "\n\n========================\n";
+
+
+	// Conditional statement for user's choose
+	if (choice == 1) {
+		pass_by_value(arr, num);
+	}
+	//else if (choice == 2) {
+	//	pass_by_address(&arr, &num);
+
+	//}
+
+
+	// Output value after sort
+	for (i = 0; i < num; i++) {
+		cout << arr[i] << " ";
+
+	}
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+// bubble sort (pass-by-value function)
+void pass_by_value(int arr[], int size)
+{
 
 
 	int i, j;
 
-	cout << "\nSorting Array Using Bubble Sort....\n";
+	cout << "\nsorting array using bubble sort....\n";
 
 
-	// Looping through the list
+	// looping through the list
 	for (i = 0; i < size; i++) {
 
 		// (size-i-1) because we don't have to loop through the last part of the array after each loop since the largest number is already at last
@@ -22,10 +91,11 @@ void Pass_by_value(int arr[], int size) {
 
 			if (arr[j] > arr[j + 1]) {
 
-				// Swapping value if the current value is larger than adjacent value
+				// swapping value if the current value is larger than adjacent value
 				int temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
+
 
 
 			}
@@ -35,30 +105,37 @@ void Pass_by_value(int arr[], int size) {
 
 
 
-int main()
-{
+// bubble sort (pass-by-address)
+void pass_by_address(int* arr[], int* size) {
 
-	int num, arr[10], i, sorted_arr;
 
-	cout << "Enter Total Number of Elements: ";
-	cin >> num;
-	cout << "\n Enter " << num << " Elements : \n";
+	int i, j;
 
-	// Letting the user to input all of the values into an array
-	for (i = 0; i < num; i++) {
-		cout << "number " << i + 1 << " : ";
-		cin >> arr[i];
+	cout << "\nsorting array using bubble sort....\n";
+
+
+	// looping through the list
+	for (i = 0; i < *size; i++) {
+
+		// (size-i-1) because we don't have to loop through the last part of the array after each loop since the largest number is already at last
+		for (j = 0; j < (*size - i - 1); j++) {
+
+			if (*arr[j] > *arr[j + 1]) {
+
+				// swapping value if the current value is larger than adjacent value
+				int temp = *arr[j];
+				*arr[j] = *arr[j + 1];
+				*arr[j + 1] = temp;
+
+
+
+			}
+		}
 	}
 
-	Pass_by_value(arr, num);
-
-	
-	for (i = 0; i < num; i++) {
-		cout << arr[i] << " ";
-
-	}
-
-
-	return 0;
 }
+
+
+
+
 
